f289fb27d8cb11b314d46974d000a293d134319b
wltjr0920 <wltjr0920@ajou.ac.kr>
2020-01-30 00:17:08 +0900

    first commit

---
+++ front/src/ApiService.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+class ApiService {
+
+    upload(data) {
+        const url = 'fashionq/upload/';
+        console.log(data)
+        return axios.post(url, data);
+    }
+}
+
+export default new ApiService();
